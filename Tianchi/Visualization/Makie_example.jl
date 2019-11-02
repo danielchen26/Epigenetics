66596af7ac891b2655fe9a6e96676c41b6e031fa
danielchen26 <chentianchi@gmail.com>
2019-11-01 23:54:34 -0400

    add 2d streamplot example

--- Tianchi/Visualization/Makie_example.jl
+++ Tianchi/Visualization/Makie_example.jl
@@ -96,3 +96,83 @@ rg = LinRange(-1.5, 1.5, 10)
  r = range(-2, stop = 2, length = 100)
 me = [f(x,y,z) for x = r ,y =r, z=r]
 contour(me)
+
+
+
+
+
+
+
+# ======= 2d streamplot =========
+using Makie
+using Random
+# struct FitzhughNagumo{T}
+#     ϵ::T
+#     s::T
+#     γ::T
+#     β::T
+# end
+Random.seed!(10)
+point(x) = Makie.Point2f0(x)
+point(x, y) = Makie.Point2f0(x, y)
+const Point = Makie.Point2f0
+
+# f(x, P::FitzhughNagumo) = Point((x[1]-x[2]-x[1]^3+P.s)/P.ϵ,
+#                                          P.γ*x[1]-x[2] + P.β)
+#
+# f(x) = f(x, P)
+
+# new f(x) with DC model
+f(x) = Point( (0.276*x[1]^2 + 1.38*x[2]^2 + 0.897*x[1]^2*x[2]^2)/(1+ x[1]^2 + x[2]^2 + x[1]^2*x[2]^2) - 0.138*x[1],
+(0.00828*x[1]^2 + 0.0828*x[2]^2 + 0.092*x[1]^2*x[2]^2)/(1+ x[1]^2 + x[2]^2 + x[1]^2*x[2]^2) - 0.046*x[2] )
+
+# P = FitzhughNagumo(0.1, 0.0, 1.5, 0.8)
+R = 1.5
+l = (32, 32)
+limits = FRect(0, 0, 10R, 10R)
+r = (range(limits.origin[1], limits.origin[1] + limits.widths[1], length=l[1]+1),
+ range(limits.origin[2], limits.origin[2] + limits.widths[2], length=l[2]+1)
+)
+p = Scene(resolution=(1600,1000), limits=limits)
+
+mask = trues(l...)
+arrows = Tuple{Point,Point}[]
+
+for c in CartesianIndices(mask)
+
+    i0, j0 = Tuple(c)
+    x0 = Point(first(r[1]) + (i0 - 0.01)*step(r[1]),
+              first(r[2]) + (j0 - 0.01)*step(r[1]))
+    dt = 0.005
+
+    if mask[c]
+        push!(arrows, (x0, f(x0)/norm(f(x0))))
+
+        mask[c] == false
+        for d in (-1,1)
+            x = x0
+            xx = [x]
+            i0, j0 = Tuple(c)
+            while x in limits && length(xx) < 500
+                x = x + d*dt*f(x)/norm(f(x))
+                if !(x in limits)
+                    break
+                end
+                i = searchsortedlast(r[1], x[1])
+                j = searchsortedlast(r[2], x[2])
+                if (i,j) != (i0, j0)
+                    if !mask[i,j]
+                        break
+                    end
+                    mask[i,j] = false
+                    i0, j0 = i, j
+                end
+                push!(xx, x)
+            end
+            lines!(p, xx)
+        end
+    end
+
+end
+arrows!(p, first.(arrows), last.(arrows), lengthscale=0.0, arrowsize=0.3)
+display(p)
